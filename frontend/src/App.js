d1ef8e08e145b64043cd20931c4af2ab72f6e599
AKSHATH P <akshathpkk@gmail.com>
2024-06-18 23:05:47 +0530

    configured frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-red-500" >HELLO WORLD</div>
+      <p ></p>
+    </div>
+  );
+}
+
+export default App;
