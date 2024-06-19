bbb09ae5eb3a0a4e8cd64b7ab23e82abecafba55
AKSHATH P <akshathpkk@gmail.com>
2024-06-19 22:17:25 +0530

    configured routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+import Register from './pages/Register.js';
+import Createpost from './pages/Createpost.js';
+import Postdetails from './pages/Postdetails.js';
+import EditPost from './pages/Editpost.js'
+import Profile from './pages/Profile.js'
+import Mypost from './pages/Mypost.js'
+import {Route , Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <div className="text-red-500" >HELLO WORLD</div>
-      <p ></p>
-    </div>
+    <>
+    <Routes>
+      <Route exact path = '/' element = {<Home/>}/>
+      <Route exact path = '/login' element = {<Login/>}/>
+      <Route exact path = '/register' element = {<Register/>}/>
+      <Route exact path = '/write' element = {<Createpost/>}/>
+      <Route exact path = '/Post/post/:id' element = {<Postdetails/>}/>
+      <Route exact path = '/edit/:id' element = {<EditPost/>}/>
+      <Route exact path = '/myblogs/:id' element = {<Mypost/>}/>
+      <Route exact path = '/profile/:id' element = {<Profile/>}/>
+    </Routes>
+    </>
   );
 }
 
